e2454323c8609959131897124406fd7ffeb831d8
tanishchugh01 <tanishchugh01@gmail.com>
2022-08-03 20:25:57 +0530

    Added delete functionality

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,25 +1,25 @@
-import {operations} from './model.js'
+import { operations } from "./model.js";
 
-window.addEventListener('load',bindEvents);
+window.addEventListener("load", bindEvents);
 
-function bindEvents(){
-  document.querySelector('#create').addEventListener('click',addStudent);
+function bindEvents() {
+  document.querySelector("#create").addEventListener("click", addStudent);
 }
 
-function addStudent(){
+function addStudent() {
   // const id=document.querySelector('#id').value;
   const id = Date.now().toString(36) + Math.random().toString(36).substr(2);
-  const name=document.querySelector('#name').value;
-  const roll=document.querySelector('#roll').value;
-  const standard=document.querySelector('#standard').value;
-  const section=document.querySelector('#section').value;
-  
-  const student =operations.create(id,name,roll,standard,section);
-  
+  const name = document.querySelector("#name").value;
+  const roll = document.querySelector("#roll").value;
+  const standard = document.querySelector("#standard").value;
+  const section = document.querySelector("#section").value;
+
+  const student = operations.create(id, name, roll, standard, section);
+
   addToTable(student);
 }
 
-function addToTable(student){
+function addToTable(student) {
   const tbody = document.querySelector("#studentList");
 
   var tr = tbody.insertRow();
@@ -29,7 +29,7 @@ function addToTable(student){
     // if (key === "markAsDeleted" || typeof student[key] === "function") {
     //   continue;
     // }
-    if(key==='id') {
+    if (key === "id") {
       continue;
     }
     var th = tr.insertCell(cellNumber);
@@ -41,17 +41,32 @@ function addToTable(student){
 
   var oper = tr.insertCell(cellNumber);
 
-  oper.appendChild(createIcon(student.id, "trash", addToTable));
-  oper.appendChild(createIcon(student.id, "pen-to-square", addToTable));
+  oper.appendChild(createIcon(student.id, "trash", deleteStudent));
+  oper.appendChild(createIcon(student.id, "pen-to-square", editStudent));
 }
 
 function createIcon(id, className, func) {
   let icon = document.createElement("i");
 
   icon.className = `fa-solid fa-${className} me-2 hand`;
-  icon.setAttribute("task-id", id);
+  icon.setAttribute("student-id", id);
   icon.setAttribute("role", "button");
   icon.addEventListener("click", func);
 
   return icon;
+}
+
+function editStudent() {
+  
+}
+
+function deleteStudent() {
+  var StudentId = this.getAttribute("student-id");
+
+  var icon = this;
+  var row = icon.parentNode.parentNode;
+  
+  const studentList=operations.delete(StudentId);
+  
+  row.replaceChildren();
 }
--- scripts/model.js
+++ scripts/model.js
@@ -1,13 +1,17 @@
 import Student from "./contruct.js";
 
-export const operations={
-  StudentList:[],
-  
-  create(id,name,roll,standard,section){
-    const newStudent=new Student(id,name,roll,standard,section);
-    
+export const operations = {
+  StudentList: [],
+
+  create(id, name, roll, standard, section) {
+    const newStudent = new Student(id, name, roll, standard, section);
+
     this.StudentList.push(newStudent);
-    console.log("Added Student",newStudent);
+    console.log("Added Student", newStudent);
     return newStudent;
-  }
-}
+  },
+
+  delete(id) {
+    this.StudentList=this.StudentList.filter((Student) => Student.id !== id);
+  },
+};
