9d7a4e685f37e9a064e122a4e27549713d0ea8e7
tanishchugh01 <tanishchugh01@gmail.com>
2022-08-04 02:44:23 +0530

    Update command working

--- scripts/controller.js
+++ scripts/controller.js
@@ -4,6 +4,7 @@ window.addEventListener("load", bindEvents);
 
 function bindEvents() {
   document.querySelector("#create").addEventListener("click", addStudent);
+  document.querySelector("#update").addEventListener("click", updateStudent);
 }
 
 function addStudent() {
@@ -26,9 +27,6 @@ function addToTable(student) {
 
   var cellNumber = 0;
   for (var key in student) {
-    // if (key === "markAsDeleted" || typeof student[key] === "function") {
-    //   continue;
-    // }
     if (key === "id") {
       continue;
     }
@@ -56,17 +54,48 @@ function createIcon(id, className, func) {
   return icon;
 }
 
-function editStudent() {
-  
-}
-
 function deleteStudent() {
   var StudentId = this.getAttribute("student-id");
-
+  
   var icon = this;
   var row = icon.parentNode.parentNode;
   
   const studentList=operations.delete(StudentId);
   
   row.replaceChildren();
+}
+
+function editStudent() {
+  var studentId = this.getAttribute("student-id");
+  
+  const studentData=operations.edit(studentId);
+  console.log(studentData)
+  
+  const name = document.querySelector("#name");
+  const roll = document.querySelector("#roll");
+  const standard = document.querySelector("#standard");
+  const section = document.querySelector("#section");
+  
+  name.value=studentData.name;
+  roll.value=studentData.roll;
+  standard.value=studentData.standard;
+  section.value=studentData.section;
+  
+  document.querySelector('#update').setAttribute("studentId",studentId);
+}
+
+function updateStudent(){
+  const id=document.querySelector('#update').getAttribute("studentId");
+  
+  const name = document.querySelector("#name").value;
+  const roll = document.querySelector("#roll").value;
+  const standard = document.querySelector("#standard").value;
+  const section = document.querySelector("#section").value;
+  
+  const studentList=operations.update(id, name, roll, standard, section);
+  
+  document.querySelector("#studentList").innerHTML="";
+  studentList.forEach(addToTable);
+  document.querySelector('#update').setAttribute("studentId","");
+  
 }
--- scripts/model.js
+++ scripts/model.js
@@ -7,11 +7,25 @@ export const operations = {
     const newStudent = new Student(id, name, roll, standard, section);
 
     this.StudentList.push(newStudent);
-    console.log("Added Student", newStudent);
     return newStudent;
   },
 
   delete(id) {
     this.StudentList=this.StudentList.filter((Student) => Student.id !== id);
   },
+  
+  edit(id){
+    return this.StudentList.find(student=>student.id===id);
+  },
+  
+  update(id, name, roll, standard, section){
+    const student=this.StudentList.find(student=>student.id===id);
+    
+    student.name=name;
+    student.roll=roll;
+    student.standard=standard;
+    student.section=section;
+    
+    return this.StudentList;
+  }
 };
